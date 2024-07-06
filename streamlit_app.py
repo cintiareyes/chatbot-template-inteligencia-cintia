33620d2f9f44d2c0cf9e16e3719d17bed5de0de8
cintiareyes <159062351+cintiareyes@users.noreply.github.com>
2024-07-06 16:34:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("Inteligencoa Artificial con IDESIE Bussiness School")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+    "Es una aplicacion para resumir y responder tus dudas de los videos de clases grabadas. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
