ca0c091e5cbefe3fd901b46d0466c3fe2677914a
cintiareyes <159062351+cintiareyes@users.noreply.github.com>
2024-07-06 17:14:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,8 @@ st.button ('Empieza aquí')
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+openai_api_key = st.text_input("OpenAI API Key (PREGUNTA AQUI)", type="password")
+
 else:
 
     # Create an OpenAI client.
